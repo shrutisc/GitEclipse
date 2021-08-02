77e29d5b2baae215e46fd9c73b4ad62f8ab2355b
schakran <shruti_chakranarayan_tp@bmc.com>
2021-08-02 18:17:03 +0530

    addition program

---
+++ src/main/java/DemoGitProject/DemoGitProject/shruti_test.java
@@ -0,0 +1,16 @@
+package DemoGitProject.DemoGitProject;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class shruti_test 
+{
+    @Test
+    public void additon()
+    {
+    	System.out.println("Addition is : "+ (3+4 ));
+    }
+}
